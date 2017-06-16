bf0c540009c7ec44bfc2333b9cbfc737e964b003
bmcminn <labs@gbox.name>
2017-06-16 01:31:56 -0500

    fix .gitignore; get routes working; define base model for unique user impressions

--- app/main.php
+++ app/main.php
@@ -2,8 +2,9 @@
 
 session_start();
 
-
 define('ROOT_DIR',      __DIR__ . "/..");
+
+define('APP_DIR',       ROOT_DIR . "/app");
 
 define('VIEWS_DIR',     ROOT_DIR . "/resources/views");
 
@@ -16,6 +17,7 @@ define('IS_PRODUCTION', $isProd);
 
 require ROOT_DIR . '/vendor/autoload.php';
 
+require APP_DIR . '/helpers.php';
 
 
 
@@ -31,16 +33,48 @@ $container = $app->getContainer();
 
 $container['view'] = function($container) {
     $view = new \Slim\Views\Twig(VIEWS_DIR, [
+        // 'cache'         => ROOT_DIR . '/_temp/views'
+        'cache'   => false
         // 'cache' => IS_PRODUCTION ? TEMP_VIEWS : false
-        'cache' => false
+        // 'auto_reload'   => true
     ]);
 
-    $view->addExtension( new \Slim\Views\TwigExtension(
-        $container->router
-    ,   $container->request->getUri()
-    ));
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    // $view->addExtension( new \Slim\Views\TwigExtension(
+    //     // $container->router
+    //     $container['router']
+    // ,   $container->request->getUri()
+    // ));
+
+    return $view;
 };
 
 
 
 require ROOT_DIR . "/app/routes.php";
+
+
+// // Create Slim app
+// $app = new \Slim\App();
+
+// // Fetch DI Container
+// $container = $app->getContainer();
+
+// // Register Twig View helper
+// $container['view'] = function ($c) {
+//     $view = new \Slim\Views\Twig('path/to/templates', [
+//         'cache' => 'path/to/cache'
+//     ]);
+
+//     // Instantiate and add Slim specific extension
+//     $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+//     $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
+
+//     return $view;
+// };
+
+
+// // Run app
+// $app->run();
--- app/routes.php
+++ app/routes.php
@@ -2,8 +2,50 @@
 
 
 
-$app->get('/', function($req, $res) {
+$app->get('/', function($req, $res, $props) {
 
-    return $this->view->render($res, 'home.twig');
+    $userAgent = isset($_SERVER['HTTP_USER_AGENT'])         ? $_SERVER['HTTP_USER_AGENT']       : '';
+    $userLangs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])    ? $_SERVER['HTTP_ACCEPT_LANGUAGE']  : '';
 
-});
+    $userHash = md5(getRealIpAddr() . $userAgent . $userLangs);
+
+
+    $model = [
+        'site' => [
+            'title' => 'Download Monitor'
+        ]
+    ,   'user' => [
+            'udid' => $userHash
+        ]
+    ,   '_GET' => $_SERVER
+    ];
+
+
+    echo "<pre>";
+    print_r($model);
+    echo "</pre>";
+
+    return $this->view->render($res, 'home.twig', $model);
+
+})->setName('home');
+
+
+
+
+
+
+
+// $app->get('/', function ($req, $res, $args) {
+//     return $this->view->render($res, 'home.twig', [
+//         'name' => 'bobo'
+//     ]);
+// })->setName('home');
+
+
+
+// // Define named route
+// $app->get('/hello/{name}', function ($req, $res, $args) {
+//     return $this->view->render($res, 'home.twig', [
+//         'name' => $args['name']
+//     ]);
+// })->setName('profile');
