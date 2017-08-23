36274936de3b71f943a7ad8882839b3cd36ce287
bmcminn <bmcminn@users.noreply.github.com>
2017-08-22 19:11:49 -0500

    starting revamp

--- _OLD/__test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $files = scandir(getcwd().'/content/audio');
-
-    array_shift($files);
-    array_shift($files);
-    print_r($files);
--- app/main.php
+++ app/main.php
@@ -3,15 +3,18 @@
 session_start();
 
 
-define('ROOT_DIR',      __DIR__ . "/..");
 
+define('ROOT_DIR',      __DIR__  . "/");
+define('CONTENT_DIR',   ROOT_DIR . getenv('CONTENT_DIR'));
+define('APP_DIR',       ROOT_DIR . "/app");
+define('TEMP_VIEWS',    APP_DIR  . "/views");
 define('VIEWS_DIR',     ROOT_DIR . "/resources/views");
-
 define('TEMP_DIR',      ROOT_DIR . "/__temp");
-define('TEMP_VIEWS',    TEMP_DIR . "/views");
+define('LOGS_DIR',      ROOT_DIR . "/__logs");
 
 
-$isProd = isset($_ENV['production']) ? $_ENV['production'] : false;
+$isProd = getenv('production') || false;
+
 define('IS_PRODUCTION', $isProd);
 
 require ROOT_DIR . '/vendor/autoload.php';
@@ -19,27 +22,9 @@ require ROOT_DIR . '/vendor/autoload.php';
 
 
 
-$app = new \Slim\App([
-    'settings' => [
-        'displayErrorDetails' => true
-    ]
-]);
 
 
 
-$container = $app->getContainer();
-
-$container['view'] = function($container) {
-    $view = new \Slim\Views\Twig(VIEWS_DIR, [
-        // 'cache' => IS_PRODUCTION ? TEMP_VIEWS : false
-        'cache' => false
-    ]);
-
-    $view->addExtension( new \Slim\Views\TwigExtension(
-        $container->router
-    ,   $container->request->getUri()
-    ));
-};
 
 
 
